510eec5c33873df7e8630b27ea4d73a859c70121
Sma1lboy <541898146chen@gmail.com>
2025-07-04 00:46:25 +0000

    Update FoxyChat to version 20250704004431
      Release: v20250704004431
      Source: https://github.com/CodeFox-Repo/foxychat/tree/v20250704004431
      Homebrew Release: https://github.com/CodeFox-Repo/homebrew-codefox/releases/tag/v20250704004431
    
      Auto-synced by GitHub Actions

--- Formula/foxychat.rb
+++ Formula/foxychat.rb
@@ -1,8 +1,8 @@
 cask "foxychat" do
-  version "0.0.15"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "20250704004431"
+  sha256 "61da417eb288ba3cff4185aa97ac03149739729026174c3ac6010854b23aebac"
   
-  url "https://github.com/CodeFox-Repo/homebrew-codefox/releases/download/v#{version}/FoxyChat-#{version}-arm64.dmg"
+  url "https://github.com/CodeFox-Repo/homebrew-codefox/releases/download/v20250704004431/FoxyChat-#{version}-arm64.dmg"
   name "FoxyChat"
   desc "All in one Chat App"
   homepage "https://github.com/CodeFox-Repo/foxychat"
